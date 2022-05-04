3b582661160bd95df45796ca96de25f510610257
changzer <changzer@gmail.com>
2022-05-04 20:33:15 -0300

    convenioService

--- src/main/java/br/com/uniamerica/api/service/ConvenioService.java
+++ src/main/java/br/com/uniamerica/api/service/ConvenioService.java
@@ -1,4 +1,38 @@
 package br.com.uniamerica.api.service;
 
+import br.com.uniamerica.api.entity.Convenio;
+import br.com.uniamerica.api.repository.ConvenioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
 public class ConvenioService {
+    private ConvenioRepository convenioRepository;
+
+    public Optional<Convenio> findById(Long id){
+        return this.convenioRepository.findById(id);
+    }
+
+    public Page<Convenio> listAll(Pageable pageable){
+        return this.convenioRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public void update(Long id, Convenio convenio){
+        if (id == convenio.getId()){
+            this.convenioRepository.save(convenio);
+        }
+        else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void insert(Convenio convenio){
+        this.convenioRepository.save(convenio);
+    }
 }
