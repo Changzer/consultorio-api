6e08cbf9333b93fb928948d480dce51982ff0037
changzer <changzer@gmail.com>
2022-05-05 21:29:21 -0300

    pacienteRepository finalmente!

--- src/main/java/br/com/uniamerica/api/repository/PacienteRepository.java
+++ src/main/java/br/com/uniamerica/api/repository/PacienteRepository.java
@@ -2,7 +2,12 @@ package br.com.uniamerica.api.repository;
 
 import br.com.uniamerica.api.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Eduardo Sganderla
@@ -11,4 +16,12 @@ import org.springframework.stereotype.Repository;
  * @version 1.0.0
  */
 @Repository
-public interface PacienteRepository extends JpaRepository<Paciente, Long> { }
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    @Modifying
+    @Query("UPDATE Paciente paciente " +
+            "SET paciente.excluido = :data " +
+            "WHERE paciente.id = :paciente")
+    public void updateDataExcluido(@Param("data") LocalDateTime dataEx,
+                                   @Param("paciente") Long idPaciente);
+}
